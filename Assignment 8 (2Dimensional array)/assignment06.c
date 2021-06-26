5b08af7606666cb81079ef5a33b5fce57b7d3b73
pawan4404 <shahpawan56@gmail.com>
2021-06-26 10:50:30 +0545

    assignment06 updated

--- Assignment 8 (2Dimensional array)/assignment06.c
+++ Assignment 8 (2Dimensional array)/assignment06.c
@@ -1,1 +1,21 @@
 //6.WAP to read elements in an array (3x3) and find the transpose of a matrix.
+#include <stdio.h>
+int main()
+{
+    int a[3][3], i, j;
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("Enter a number : ");
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+            printf("%d",a[j][i]);
+        printf("\n");
+    }
+    return 0;
+}
