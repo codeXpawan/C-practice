c0be96ece3797bab1713036e1abd0dcb7159e6a4
pawan4404 <shahpawan56@gmail.com>
2021-06-26 10:36:56 +0545

    assignment 5 updated

--- Assignment 8 (2Dimensional array)/assignment05.c
+++ Assignment 8 (2Dimensional array)/assignment05.c
@@ -1,1 +1,19 @@
 //5.WAP to read elements in an array (3x3) and calculate the sum of diagonal matrix.
+#include <stdio.h>
+
+int main()
+{
+    int a[3][3], i, j, sum = 0;
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("Enter the number : ");
+            scanf("%d",&a[i][j]);
+            if (i==j)
+                sum += a[i][j];
+        }
+    }
+    printf("The sum of the diagnol matrix is %d\n",sum);
+    return 0;
+}
